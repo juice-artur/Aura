96da4d211274980a8d3eecaf59a2c9e8eec2434b
juice-artur <asasin.lol10@gmail.com>
2025-08-14 22:34:25 +0300

    feat: create custom effect context

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -49,6 +49,7 @@ void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
 		FHitResult HitResult;
 		HitResult.Location = ProjectileTargetLocation;
 		EffectContextHandle.AddHitResult(HitResult);
+		
 		const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), SourceASC->MakeEffectContext());
 		
 		const FAuraGameplayTags GameplayTags = FAuraGameplayTags::Get();

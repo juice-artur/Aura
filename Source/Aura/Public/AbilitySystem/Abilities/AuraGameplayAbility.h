7926d9d29172a04389f7d8a23b80672460e41230
juice-artur <asasin.lol10@gmail.com>
2025-10-04 16:11:18 +0300

    feat: add cost and cool down in spell description

--- Source/Aura/Public/AbilitySystem/Abilities/AuraGameplayAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraGameplayAbility.h
@@ -16,6 +16,10 @@ public:
 	virtual FString GetNextLevelDescription(int32 Level);
 	static FString GetLockedDescription(int32 Level);
 	
+protected:
+	float GetManaCost(float InLevel = 1.f) const;
+	float GetCooldown(float InLevel = 1.f) const;
+	
 public:
 	UPROPERTY(EditDefaultsOnly, Category = "Input")
 	FGameplayTag StartupInputTag;

4bc39d0f3fec02afb19032d47751d2548b9db89c
juice-artur <asasin.lol10@gmail.com>
2025-10-05 20:24:02 +0300

    feat: add debuff tags

--- Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -6,7 +6,7 @@
 
 FString UAuraFireBolt::GetDescription(int32 Level)
 {
-	const int32 Damage = GetDamageByDamageType(Level, FAuraGameplayTags::Get().Damage_Fire);
+	const int32 ScaledDamage = Damage.GetValueAtLevel(Level);
 	const float ManaCost = FMath::Abs(GetManaCost(Level));
 	const float Cooldown = GetCooldown(Level);
 	if (Level == 1)
@@ -33,7 +33,7 @@ FString UAuraFireBolt::GetDescription(int32 Level)
 		                       Level,
 		                       ManaCost,
 		                       Cooldown,
-		                       Damage);
+		                       ScaledDamage);
 	}
 	else
 	{
@@ -61,13 +61,13 @@ FString UAuraFireBolt::GetDescription(int32 Level)
 		                       ManaCost,
 		                       Cooldown,
 		                       FMath::Min(Level, NumProjectiles),
-		                       Damage);
+		                       ScaledDamage);
 	}
 }
 
 FString UAuraFireBolt::GetNextLevelDescription(int32 Level)
 {
-	const int32 Damage = GetDamageByDamageType(Level, FAuraGameplayTags::Get().Damage_Fire);
+	const int32 ScaledDamage = Damage.GetValueAtLevel(Level);
 	const float ManaCost = FMath::Abs(GetManaCost(Level));
 	const float Cooldown = GetCooldown(Level);
 	return FString::Printf(TEXT(
@@ -94,5 +94,5 @@ FString UAuraFireBolt::GetNextLevelDescription(int32 Level)
 	                       ManaCost,
 	                       Cooldown,
 	                       FMath::Min(Level, NumProjectiles),
-	                       Damage);
+	                       ScaledDamage);
 }
